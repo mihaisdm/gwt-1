df20144679aeca5a6a45b69a4db9eb3e8fe508ac
Colin Alworth <niloc132@gmail.com>
2015-12-03 18:45:46 -0600

    Map methods from Java 8, based on Javadoc reference implementations
    Change-Id: I3e7e774c069ebf61f7b30f5ebbb144f973f5d46e

--- user/super/com/google/gwt/emul/java/util/Map.java
+++ user/super/com/google/gwt/emul/java/util/Map.java
@@ -18,6 +18,10 @@ package java.util;
 import static javaemul.internal.InternalPreconditions.checkCriticalNotNull;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Abstract interface for maps.
@@ -89,4 +93,113 @@ public interface Map<K, V> {
   int size();
 
   Collection<V> values();
+
+  default V compute(K key, BiFunction<? super K,? super V,? extends V> remappingFunction) {
+    V oldValue = get(key);
+    V newValue = remappingFunction.apply(key, oldValue);
+    if (oldValue != null) {
+      if (newValue != null) {
+        put(key, newValue);
+      } else {
+        remove(key);
+      }
+    } else {
+      if (newValue != null) {
+        put(key, newValue);
+      }
+    }
+    return newValue;
+  }
+
+  default V computeIfAbsent(K key, Function<? super K,? extends V> mappingFunction) {
+    V currentValue = get(key);
+    if (currentValue == null) {
+      V newValue = mappingFunction.apply(key);
+      if (newValue != null) {
+        put(key, newValue);
+      }
+      return newValue;
+    }
+    return currentValue;
+  }
+
+  default V computeIfPresent(K key, BiFunction<? super K,? super V,? extends V> remappingFunction) {
+    V oldValue = get(key);
+    if (oldValue != null) {
+      V newValue = remappingFunction.apply(key, oldValue);
+      if (newValue != null) {
+        put(key, newValue);
+      } else {
+        remove(key);
+      }
+      return newValue;
+    }
+    return null;
+  }
+
+  default void forEach(BiConsumer<? super K,? super V> action) {
+    for (Map.Entry<K, V> entry : entrySet()) {
+      action.accept(entry.getKey(), entry.getValue());
+    }
+  }
+
+  default V getOrDefault(Object key, V defaultValue) {
+    if (containsKey(key)) {
+      return get(key);
+    } else {
+      return defaultValue;
+    }
+  }
+
+  default V merge(K key, V value, BiFunction<? super V,? super V,? extends V> remappingFunction) {
+    V oldValue = get(key);
+    V newValue = (oldValue == null) ? value :
+        remappingFunction.apply(oldValue, value);
+    if (newValue == null) {
+      remove(key);
+    } else {
+      put(key, newValue);
+    }
+    return newValue;
+  }
+
+  default V putIfAbsent(K key, V value) {
+    V v = get(key);
+    if (v == null) {
+      v = put(key, value);
+    }
+
+    return v;
+  }
+
+  default boolean remove(Object key, Object value) {
+    if (containsKey(key) && Objects.equals(get(key), value)) {
+      remove(key);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  default V replace(K key, V value) {
+    if (containsKey(key)) {
+      return put(key, value);
+    } else
+      return null;
+  }
+
+  default boolean replace(K key, V oldValue, V newValue) {
+    if (containsKey(key) && Objects.equals(get(key), oldValue)) {
+      put(key, newValue);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  default void replaceAll(BiFunction<? super K,? super V,? extends V> function) {
+    for (Map.Entry<K, V> entry : entrySet()) {
+      entry.setValue(function.apply(entry.getKey(), entry.getValue()));
+    }
+  }
 }
